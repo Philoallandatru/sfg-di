5cb75d86bd85db507b5db1a3a1de849011517799
tianqin <philoallandatruly@outlook.com>
2021-03-06 20:13:01 +0800

    controller

--- src/main/java/com/spring/sfgdi/SfgDiApplication.java
+++ src/main/java/com/spring/sfgdi/SfgDiApplication.java
@@ -1,13 +1,17 @@
 package com.spring.sfgdi;
 
+import com.spring.sfgdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+		MyController myController = (MyController) ctx.getBean("MyController");
+		System.out.println(myController.sayHello());
 	}
 
 }
